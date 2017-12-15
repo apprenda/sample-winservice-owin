b759040c016671d3c8a01b2f0b154e4c0de2a3c2
Erik L <elustgarten@apprenda.com>
2017-12-15 11:13:00 -0500

    Initial commit

---
+++ src/Apprenda.Sample.OWIN/myApi.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apprenda.Sample.OWIN
+{
+    partial class myApi : ServiceBase
+    {
+        private IDisposable _webapp;
+        public myApi()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _webapp = WebApp.Start<Startup>(string.Format("http://+:{0}", 
+                ConfigurationManager.AppSettings["HTTPPort"]));
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
